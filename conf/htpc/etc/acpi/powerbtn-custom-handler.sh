3ccdd1c005d8414c2d1ed98d34a3ab16313d43ce
arnova <arnova@void.org>
2024-11-22 15:03:12 +0100

    updated: powerbtn-custom-handler.sh

--- conf/htpc/etc/acpi/powerbtn-custom-handler.sh
+++ conf/htpc/etc/acpi/powerbtn-custom-handler.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-systemctl restart kodi.service
+if systemctl is-active --quiet kodi.service; then
+  systemctl restart kodi.service
+elif systemctl is-active --quiet kodi-gbm.service; then
+  systemctl restart kodi-gbm.service
+elif systemctl is-active --quiet kodi-flatpack-gbm.service; then
+  systemctl restart kodi-flatpack-gbm.service
+fi
 
